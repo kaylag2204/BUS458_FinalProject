f6527c71bcd1db6f24564cdd49b59f40594e9de8
kaylag2204 <kaylamgordon2204@gmail.com>
2025-12-07 20:56:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import sklearn
 # Load Model
 # -----------------------------------------------------
 try:
-    with open("loan_model.pkl", "rb") as file:
+    with open("my_model.pkl", "rb") as file:
         model = pickle.load(file)
 
     # Check if model has feature names
